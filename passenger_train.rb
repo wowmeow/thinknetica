c56ebb89557d85123fe549ef035ca7cb7c76a0c3
wowmeow <anna.shigirdanova@yandex.ru>
2021-04-17 20:03:54 +0300

    update init method

--- passenger_train.rb
+++ passenger_train.rb
@@ -1,9 +1,12 @@
 class PassengerTrain < Train
+  include InstanceCounter
+
   attr_reader :type
 
   def initialize(number)
     super(number, wagons)
     @type = initial_type
+    register_instance
   end
 
   def initial_type
